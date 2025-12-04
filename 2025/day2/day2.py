e93c9b198fb52f178d2c438ad55c07d985caa2b7
loganbrinsmead <loganbrinsmead1115@gmail.com>
2025-12-04 11:19:10 -0800

    day 2 part 2

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -13,26 +13,30 @@ with open("input.txt", "r") as inputFile:
 
             # lastId + 1 because it is inclusive
             for num in range(firstId, lastId + 1):
+                foundRes = False
 
                 window = ""
                 strNum = str(num)
+
+                
 
                 # checking the current number for repeated sequences
                 for i, c in enumerate(strNum):
                     if len(window) >= 1 and window[0] == c:
                         # loop through the rest of the window and the current number, checking for a duplicate
-                        secondWindow = c
-                        for j in range(i + 1, len(strNum)):
+                        secondWindow = ""
+                        for j in range(i, len(strNum)):
                             secondWindow += strNum[j]
-
-                        if len(secondWindow) and secondWindow == window:
-                            print("FOUND WINDOW! window: ", window, " ,secondWindow: ", secondWindow, ", strNum: ", strNum)
-                            res += num
-                            break
+                            if secondWindow == window and j == len(strNum) - 1:
+                                res += num
+                                foundRes = True
+                                break   
+                            elif secondWindow == window:
+                                secondWindow = ""
+                            
                     
-                    # if len(strNum) >= i + 2 and strNum[i + 1] == c:
-                    #     res += num
-                    #     break
+                    if foundRes:
+                        break
 
                     window += c
                     
