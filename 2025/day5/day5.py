f9a6ad3210786ee8589a60f9b563273c195ef2d7
loganbrinsmead <loganbrinsmead1115@gmail.com>
2025-12-05 09:32:38 -0800

    Day 5 part 2

--- 2025/day5/day5.py
+++ 2025/day5/day5.py
@@ -5,6 +5,7 @@ processIngredients = False
 ranges = []
 
 res = 0
+partTwoRes = 0
 
 with open("./input.txt", "r") as file:
     
@@ -15,7 +16,7 @@ with open("./input.txt", "r") as file:
         
         if not processIngredients:
             nums = line.strip().split('-')
-            ranges.append((int(nums[0]), int(nums[1])))
+            ranges.append([int(nums[0]), int(nums[1])])
         else:
             for range in ranges:
                 # if this is true it is in the range
@@ -23,5 +24,22 @@ with open("./input.txt", "r") as file:
                 if integer >= range[0] and integer <= range[1]:
                     res += 1
                     break
+        
+        # merge overlapping intervals
+        ranges.sort()
+        filteredRanges = [ranges[0]]
+        for start, end in ranges[1:]:
+            #  get the ending value of the last sorted interval
+             lastEnd = filteredRanges[-1][1]
+
+            #  if true, this means that the intervals are overlapping, so we just take the max ending value. otherwise they don't overlap, so just append.
+             if start <= lastEnd:
+                  filteredRanges[-1][1] = max(lastEnd, end)
+             else:
+                  filteredRanges.append([start, end])
+                 
+
+    for range in filteredRanges:    
+            partTwoRes += (range[1] - range[0]) + 1
             
-print(res)
+print(res, partTwoRes)
