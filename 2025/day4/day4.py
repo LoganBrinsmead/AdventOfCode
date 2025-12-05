8bded505c7989a37c68496a63845170f34369264
loganbrinsmead <loganbrinsmead1115@gmail.com>
2025-12-04 20:06:21 -0800

    day 4 part 2

--- 2025/day4/day4.py
+++ 2025/day4/day4.py
@@ -20,31 +20,36 @@ with open("./input.txt", "r") as file:
         matrix.append(row)
 
 res = 0
+canRemoveRolls = True
 
-for row in range(len(matrix)):
-    for col in range(len(matrix[row])):
+while canRemoveRolls:
+    canRemoveRolls = False
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
 
-        # don't count empty cells
-        if matrix[row][col] == '.':
-            continue
-
-        countAdjacent = 0
-        for dir in directions:
-            dr, dc = dir[0], dir[1]
-            curDirection = (row + dr, col + dc)
-            if (
-            curDirection[0] < 0 or
-            curDirection[0] >= len(matrix) or
-            curDirection[1] < 0 or
-            curDirection[1] >= len(matrix[row])):
+            # don't count empty cells
+            if matrix[row][col] == '.':
                 continue
 
-            if matrix[curDirection[0]][curDirection[1]] == '@':
-                countAdjacent += 1
+            countAdjacent = 0
+            for dir in directions:
+                dr, dc = dir[0], dir[1]
+                curDirection = (row + dr, col + dc)
+                if (
+                curDirection[0] < 0 or
+                curDirection[0] >= len(matrix) or
+                curDirection[1] < 0 or
+                curDirection[1] >= len(matrix[row])):
+                    continue
+
+                if matrix[curDirection[0]][curDirection[1]] == '@':
+                    countAdjacent += 1
+                
             
-        
-        if countAdjacent < 4:
-            res += 1
+            if countAdjacent < 4:
+                res += 1
+                matrix[row][col] = '.'
+                canRemoveRolls = True
         
 
 print(res)
