f13342a4ebe4146435dfe9940f97d4abe2cae744
loganbrinsmead <loganbrinsmead1115@gmail.com>
2025-12-06 20:50:05 -0800

    day 6 part 2

--- 2025/day6/day6.py
+++ 2025/day6/day6.py
@@ -1,10 +1,37 @@
-data = []
+
+prep = []
 
 with open("./input.txt", "r") as file:
 
     for line in file:
-        line = line.strip().split()
-        data.append(line)
+        line = line.replace("\n", "")
+        line = list(line)
+        prep.append(line)
+
+data = []
+colCount = len(prep[0]) - 1
+curValue = ''
+curRow = []
+
+# if current symbol is an operation, append current values to matrix
+while colCount > -1:
+    if curValue.isdigit():
+        curRow.append(curValue)
+    curValue = ''
+
+    for row in range(len(prep)):
+        if prep[row][colCount] == '*' or prep[row][colCount] == '+':
+            if curValue.isdigit():
+                curRow.append(curValue)
+                curValue = ''
+
+            curRow.append(prep[row][colCount])
+            data.append(curRow)
+            curRow = []
+        elif prep[row][colCount].isdigit():
+            curValue += prep[row][colCount]
+    
+    colCount -= 1
 
 res = 0
 curData = 0
@@ -14,16 +41,14 @@ operations = {
     '+':  lambda x, y : x + y
 }
 
-# start from the bottom to get the operation, then loop through the column to the top and apply the operation
-for col in range(len(data[0])):
-    for row in range(len(data) - 1, -1, -1):
-        if row == len(data) - 1:
-            operation = data[row][col]
-            if operation == '*':
-                curData += 1
-        else:
-            print(curData, data[row][col])
-            curData = operations[operation](curData, int(data[row][col]))
+for row in data:
+    operation = row[len(row) - 1]
+    if operation == '*':
+        curData = 1
+
+
+    for i in range(len(row) - 1):
+        curData = operations[operation](curData, int(row[i]))
     
     res += curData
     curData = 0
