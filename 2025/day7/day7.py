97046c89595f9b3b455a3fcec8035620eab06224
loganbrinsmead <loganbrinsmead1115@gmail.com>
2025-12-08 15:19:15 -0800

    day 7 part 2

--- 2025/day7/day7.py
+++ 2025/day7/day7.py
@@ -7,25 +7,54 @@ with open("./input.txt", "r") as file:
         data.append(line)
 
 
-res = 0
+def part1():
 
-# we can track the columns that have beams in them using caching
-# stores index of column
-beamCols = set()
-beamCols.add(data[0].index('S'))
+    res = 0
 
-for rowI, row in enumerate(data):
-    for col in range(len(row)):
-        if row[col] == "^" and col in beamCols:
-            beamCols.remove(col)
-            res += 1
-            if col - 1 >= 0 and data[rowI][col] != '|':
-                data[rowI][col - 1] = '|'
-                beamCols.add(col - 1)
-            if col + 1 < len(row) and data[rowI][col + 1] != '|':
-                beamCols.add(col + 1)
-                data[rowI][col + 1] = '|'
-            
-            print(res)
+    # we can track the columns that have beams in them using caching
+    # stores index of column
+    beamCols = set()
+    beamCols.add(data[0].index('S'))
 
-print(res)
+    for rowI, row in enumerate(data):
+        for col in range(len(row)):
+            if row[col] == "^" and col in beamCols:
+                beamCols.remove(col)
+                res += 1
+                if col - 1 >= 0 and data[rowI][col] != '|':
+                    data[rowI][col - 1] = '|'
+                    beamCols.add(col - 1)
+                if col + 1 < len(row) and data[rowI][col + 1] != '|':
+                    beamCols.add(col + 1)
+                    data[rowI][col + 1] = '|'
+                
+    return res
+
+def part2():
+
+    # col index: number of timelines currently running through that column
+    beamCols = {data[0].index('S'): 1}
+
+    for rowI, row in enumerate(data):
+        for col in range(len(row)):
+            if row[col] == "^" and col in beamCols:
+                print(beamCols)
+                prev = col - 1
+                next = col + 1
+                if prev >= 0:
+                    if prev in beamCols:
+                        beamCols[prev] += beamCols[col]
+                    else:
+                        beamCols[prev] = beamCols[col]
+                if next < len(row):
+                    if next in beamCols:
+                        beamCols[next] += beamCols[col]
+                    else:
+                        beamCols[next] = beamCols[col]
+
+                del beamCols[col]
+
+    
+    return sum(beamCols.values())
+
+print(part2())
